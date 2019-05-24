63e9df719a6eefe688e1dca29a7ac0a10811f3c7
Federico <f.a.s.oac@gmail.com>
2019-05-24 19:09:15 -0300

    implementacion de format 2 de gadget!

--- libs/utils.py
+++ libs/utils.py
@@ -55,7 +55,7 @@ def save_particles(ids, pos, vel, mass, u, outfile, format):
     elif format == 1:
         Ngas = len(mass)
         Npart = np.array([Ngas, 0, 0, 0, 0, 0])
-        Nmass = np.array([0, 0, 0, 0, 0, 0])
+        Nmass = np.array([mass[0], 0, 0, 0, 0, 0])
         # Linearizing the 3D-array of the position and velocity
         pos = pos.ravel()
         vel = vel.ravel()
@@ -107,6 +107,99 @@ def save_particles(ids, pos, vel, mass, u, outfile, format):
 
             # Energy
             nbytes = len(u) * 4
+            f.write(pack('i', nbytes))
+            f.write(pack('f' * len(u), *u))
+            f.write(pack('i', nbytes))
+
+    elif format == 2:
+        Ngas = len(mass)
+        Npart = np.array([Ngas, 0, 0, 0, 0, 0])
+        Nmass = np.array([mass[0], 0, 0, 0, 0, 0])
+        # Linearizing the 3D-array of the position and velocity
+        pos = pos.ravel()
+        vel = vel.ravel()
+
+        dummy = np.zeros(Npart[0])
+        time = 0.
+        redshift = 0.0  # double
+        flag_sfr = 0  # long
+        flag_feedback = 0  # long
+        bytesleft = 256 - 6*4 - 6*8 - 8 - 8 - 2*4 - 6*4
+        fill = np.zeros(int(bytesleft/4.0), dtype=np.int)  # int
+
+
+        with open(outfile, 'wb') as f:
+            
+            nbytes  = 256
+            nbytes4 = 8
+            
+            f.write(pack('i', nbytes4))
+            f.write(b'HEAD')
+            f.write(pack('i', nbytes + 8))
+            f.write(pack('i', nbytes4))
+            # Header
+            
+            f.write(pack('i', nbytes))
+            f.write(pack('i' * len(Npart), *Npart))
+            f.write(pack('d' * len(Nmass), *Nmass))
+            f.write(pack('d', time))
+            f.write(pack('d', redshift))
+            f.write(pack('i', flag_sfr))
+            f.write(pack('i', flag_feedback))
+            f.write(pack('i' * len(Npart), *Npart))
+            f.write(pack('i' * len(fill), *fill))
+            f.write(pack('i', nbytes))
+
+            # Positions
+            nbytes = int(len(pos) * 4)
+            f.write(pack('i', nbytes4))
+            f.write(b'POS ')
+            f.write(pack('i', nbytes + 8))
+            f.write(pack('i', nbytes4))
+            
+            f.write(pack('i', nbytes))
+            f.write(pack('f' * len(pos), *pos))
+            f.write(pack('i', nbytes))
+
+           # Velocities
+            f.write(pack('i', nbytes4))
+            f.write(b'VEL ')
+            f.write(pack('i', nbytes + 8))
+            f.write(pack('i', nbytes4))
+            
+            f.write(pack('i', nbytes))
+            f.write(pack('f' * len(vel), *vel))
+            f.write(pack('i', nbytes))
+
+            # Ids
+            nbytes = int(len(ids) * 4)
+            f.write(pack('i', nbytes4))
+            f.write(b'ID  ')
+            f.write(pack('i', nbytes + 8))
+            f.write(pack('i', nbytes4))
+
+            f.write(pack('i', nbytes))
+            f.write(pack('i' * len(ids), *ids))
+            f.write(pack('i', nbytes))
+
+            # Masses
+            nbytes = int(len(mass) * 4)
+            f.write(pack('i', nbytes4))
+            f.write(b'MASS')
+            f.write(pack('i', nbytes+8))
+            f.write(pack('i', nbytes4))
+            
+            f.write(pack('i', nbytes))
+            f.write(pack('f' * len(mass), *mass))
+            f.write(pack('i', nbytes))
+
+            # Energy
+            nbytes = int(len(u) * 4)
+            f.write(pack('i', nbytes4))
+            f.write(b'U   ')
+            f.write(pack('i', nbytes + 8))
+            f.write(pack('i', nbytes4))
+
             f.write(pack('i', nbytes))
             f.write(pack('f' * len(u), *u))
             f.write(pack('i', nbytes))
