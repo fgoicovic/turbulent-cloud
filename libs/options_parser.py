0a3d9d90d980e9909e73edeeb2676f75cbefe3f3
Emmanuel Gianuzzi <egianuzzi@gianuzzi.iate.oac.uncor.edu>
2020-09-17 00:59:16 -0300

    Added param beta. Added Default in help

--- libs/options_parser.py
+++ libs/options_parser.py
@@ -9,13 +9,13 @@ class OptionsParser:
         self.parser.add_argument("-N", "-n",
                             dest     = "num",
                             type     = int,
-                            help     = "Number of particles (required)",
+                            help     = "Number of particles (required).",
                             required = True)
 
         self.parser.add_argument("-o",
                             metavar = "outfile",
                             dest    = "outfile",
-                            help    = "Name of output file",
+                            help    = "Name of output file.",
                             default = "ics_cloud.dat")
 
         self.parser.add_argument("-format",
@@ -23,40 +23,54 @@ class OptionsParser:
                             type    = int,
                             help    = "Format of output file.\n0 = ASCII,\n"+\
                                        "1 = Gadget binary format 1,\n"+\
-                                       "2 = Gadget binary format 2",
+                                       "2 = Gadget binary format 2. \n"+\
+                                       " [Default = 1]",
                             default = 1)
 
         self.parser.add_argument("-m", "-mass",
                             dest     = "mass",
                             type     = float,
-                            help     = "Total gas mass (in solar masses)",
+                            help     = "Total gas mass (in solar masses).\n"+\
+                                       " [Default = 1]",
                             default  = 1.)
 
         self.parser.add_argument("-npow",
                             dest     = "npow",
                             type     = float,
-                            help     = "Power index of the power spectrum",
+                            help     = "Power index of the power spectrum.\n"+\
+                                       " [Default = -4]",
                             default  = -4.)
 
         self.parser.add_argument("-ngrid",
                             dest     = "ngrid",
                             type     = int,
                             help     = "Number of grid points per dimension for\n"+\
-                                        "the turbulent velocity field",
+                                       "the turbulent velocity field.\n"+\
+                                       " [Default = 256]",
                             default  = 256)
 
         self.parser.add_argument("-r", "-radius",
                             dest     = "radius",
                             type     = float,
-                            help     = "Radius of the sphere (in parsecs)",
+                            help     = "Radius of the sphere (in parsecs).\n"+\
+                                       " [Default = 1]",
                             default  = 1.)
 
         self.parser.add_argument("-a", "-alpha",
                             dest     = "alpha",
                             type     = float,
-                            help     = "Ratio of kinetic energy to the\n"+\
-                                        "magnitude of gravitational energy",
+                            help     = "Ratio of turbulent energy to the\n"+\
+                                       "magnitude of gravitational energy.\n"+\
+                                       " [Default = 0.5]",
                             default  = 0.5)
+
+        self.parser.add_argument("-b", "-beta",
+                            dest     = "beta",
+                            type     = float,
+                            help     = "Ratio of rotational energy to the\n"+\
+                                       "magnitude of gravitational energy.\n"+\
+                                       " [Default = None]",
+                            default  = -1)
 
         self.parser.add_argument("--units",
                             dest     = "units",
